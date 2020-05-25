de82d8178ceb6de67fda72cf13c3c1c9c9f94ce1
agutin <agustin124@gmail.com>
2020-05-25 12:11:15 -0300

    version 1

---
+++ src/Polinomio/Polynomial.java
@@ -0,0 +1,137 @@
+package Polinomio;
+import java.util.NoSuchElementException;
+
+public class Polynomial {
+	
+	private Node root;
+	private int count;
+
+	
+	public Polynomial() {
+		this.add(0,0);
+		
+	}
+	
+	public Polynomial(int[] coef) {
+		int len = coef.length;
+		for (int i = 0; i < coef.length ; i++) {
+			if (coef[i] != 0) {
+				this.add(coef[i], i);
+				count ++;
+			}	
+		}
+	}
+	
+	public int countValue() {
+		return count;
+	}
+	
+	public void add(int x, int e) {
+		Node n = new Node(x,e);
+		n.next = root;
+		root = n;
+	
+		}
+	
+	public Polynomial polynomialAdd (Polynomial Pol) {
+		
+		Node p = root;
+		
+		while(p != null) {
+			p.value += Pol.getCoefficient(p.exponent);
+			p = p.next;
+		}
+		
+		return this;
+	}
+	
+	public int getCoefficient(int index) {
+		if(index<0 || index > count) {
+			throw new NoSuchElementException();
+		}
+		
+		Node p = root;
+		while(p != null && p.exponent != index) {
+			p = p.next;
+		}
+		return p.value;
+	}
+	
+	public void setCoefficient(int x, int coef) {
+		
+		Node p = root;
+		while(p.exponent != x) {
+			p = p.next;
+		}
+		
+		p.value = coef;
+	}
+	
+	public float valueOf(float x) {
+		float total = 0;
+		Node p = root;
+		while(p != null) {
+			total += p.value * Math.pow(x,p.exponent);
+			p = p.next;
+		}
+		
+		return total;
+	}
+	
+	public boolean equals(Object x) {
+		Polynomial e = (Polynomial) x;
+		Node p = root;
+		boolean flat = false;
+		if (count == e.countValue()) {
+			flat = true;
+			while (p != null && flat) {
+				if(p.value == e.getCoefficient(p.exponent) ) {
+					p = p.next;
+				}else {
+					flat = false;
+					break;
+				}
+			}
+		}
+		if (flat) {
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("[");
+		Node p = root;
+		while(p != null) {
+			sb.append("[");
+			sb.append(p.value);
+			sb.append("X^");
+			sb.append(p.exponent);
+			sb.append("]");
+			sb.append(", ");
+			p = p.next;
+		}
+		sb.append("]");
+		return sb.toString();
+	}
+
+	
+	private static class Node{
+		
+		int value;
+		
+		int exponent;
+		
+		Node next;
+		
+		public Node(int value, int exponent){
+			
+			this.value = value;
+			this.exponent = exponent;
+			
+		}
+		
+	}
+	
+}
---
+++ src/Polinomio/PolynomialTest.java
@@ -0,0 +1,85 @@
+package Polinomio;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+
+class PolynomialTest {
+	
+	private Polynomial polinomio;
+	
+	@BeforeEach
+	public void setup() {
+		int array[] = {5,3};
+		polinomio = new Polynomial(array);
+	}
+	
+	@Test
+	public void testPolynomioCero() {
+		Polynomial polinomioCero = new Polynomial();
+		assertEquals("[[0X^0], ]",polinomioCero.toString());
+		
+	}
+	
+	@Test
+	public void testPolynomialWithZero(){
+		int array0[] = {5,0,4};
+		Polynomial polinomio0 = new Polynomial(array0);
+		assertEquals("[[4X^2], [5X^0], ]",polinomio0.toString());
+		
+	}
+	
+	@Test
+	public void testPolynomialAdd() {
+		int array2[] = {4,2}; 
+		Polynomial polinomio2 = new Polynomial(array2);
+		Polynomial suma = polinomio.polynomialAdd(polinomio2);
+		assertEquals("[[5X^1], [9X^0], ]", suma.toString());
+		
+	}
+	
+	
+	@Test
+	public void testSetCoefficient() {
+		polinomio.setCoefficient(1, 4);
+		assertEquals("[[4X^1], [5X^0], ]", polinomio.toString());
+	}	
+	
+	@Test
+	public void testGetCoefficient() {
+	
+		assertEquals(3, polinomio.getCoefficient(1));
+	}
+	
+	@Test
+	public void testValueOf() {
+		assertEquals(11, polinomio.valueOf(2));
+	}
+	
+	@Test
+	public void testEqualsTrue() {
+		int array3[] = {5,3};
+		Object polinomio3 = new Polynomial(array3);
+		assertEquals(true, polinomio.equals(polinomio3));
+	
+		
+	}
+	
+	@Test
+	public void testEqualsFalse() {
+		int array3[] = {5,4,3};
+		Object polinomio3 = new Polynomial(array3);
+		assertEquals(false, polinomio.equals(polinomio3));
+	}
+	
+	
+	@Test
+	public void testToString() {
+	
+		assertEquals("[[3X^1], [5X^0], ]", polinomio.toString());
+	}
+
+}
